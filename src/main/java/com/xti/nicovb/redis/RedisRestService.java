378513027a685db8dc78cbe96e5dc58726a24965
nicovanbelle <nicovanbelle@gmail.com>
2014-06-24 09:12:26 +0200

    trying transaction

--- src/main/java/com/xti/nicovb/redis/RedisRestService.java
+++ src/main/java/com/xti/nicovb/redis/RedisRestService.java
@@ -1,5 +1,7 @@
 package com.xti.nicovb.redis;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -11,6 +13,8 @@ import org.springframework.stereotype.Component;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Response;
+import redis.clients.jedis.Transaction;
 
 @Component
 @Produces("text/plain")
@@ -24,10 +28,24 @@ public class RedisRestService {
 	@Path("/{key}/{value}")
 	public String addValue(@PathParam("key") String key,
 			@PathParam("value") String value) {
+
 		Jedis jedis = pool.getResource();
-		Long valueLength = jedis.append(key, value);
+
+		// Start transaction
+		Transaction transaction = jedis.multi();
+
+		/*
+		 * NX -- Only set the key if it does not already exist. XX -- Only set
+		 * the key if it already exist.
+		 */
+		Response<String> set = transaction.set(key, value, "NX");
+
+		// End transaction
+		List<Object> execGetResponse = transaction.exec();
+
 		pool.returnResource(jedis);
-		return String.format("Total length of the key is now %d", valueLength);
+		return String.format("Total length of the key is now %s (%s)",
+				set.get(), execGetResponse.get(0));
 	}
 
 	@GET

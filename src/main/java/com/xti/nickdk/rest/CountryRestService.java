ae70c8e72e7ed37996bf12fb0f674d29cf85fe5e
Jens De Temmerman <jensdt@Jenss-MacBook-Pro-2.local>
2014-06-25 09:08:51 +0200

    Added logging through SLF4J/logback

--- src/main/java/com/xti/nickdk/rest/CountryRestService.java
+++ src/main/java/com/xti/nickdk/rest/CountryRestService.java
@@ -5,6 +5,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.xti.nickdk.resources.Country;
@@ -14,10 +16,14 @@ import com.xti.nickdk.resources.Country;
 @Produces("application/json")
 @Path("/country")
 public class CountryRestService {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(CountryRestService.class);
 
 	@GET
 	@Path("/{id}") 
 	public Country getCountry(@PathParam("id") String id) {
+		LOGGER.info("Calling country service for ID {}", id);
+		
 		Country country = new Country(id);
 		country.setName("Belgium");
 		return country;

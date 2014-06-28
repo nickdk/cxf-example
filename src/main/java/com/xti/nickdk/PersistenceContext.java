875074b0cef3977c1c7841007927dfa9063314c7
Nick De Kock <nick.dekock@cronos.be>
2014-06-28 11:06:49 +0200

    Added postgresql dialect

--- src/main/java/com/xti/nickdk/PersistenceContext.java
+++ src/main/java/com/xti/nickdk/PersistenceContext.java
@@ -78,7 +78,7 @@ public class PersistenceContext{
    Properties additionalProperties() {
       Properties properties = new Properties();
       properties.setProperty("hibernate.hbm2ddl.auto", "update");
-      properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+      properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
       return properties;
    }
 }
